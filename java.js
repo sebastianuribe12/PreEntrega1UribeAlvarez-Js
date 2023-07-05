b4eabbfabe0118b6cebac94e9bd02b80fe73fed4
sebastianuribe12 <urisbe@gmail.com>
2023-07-04 22:03:04 -0500

    Se crea el codigo basico con el que se trabajara

--- java.js
+++ java.js
@@ -1,1 +1,14 @@
-alert("- Ingrese a la consola")
+alert("Calculador de promedio de edades ✌️\n -----------por Sebastian Uribe Alvarez-----------");
+
+console.log("-------------------");
+let NumPer = parseInt(prompt("cuantas personas registrara"));//numero de personas cA
+let RegEd;//Registro de las edades eA
+let AcumEd = 0; //acumulador de edades sA
+let n;//ciclo
+
+for(n=1; n<=NumPer;n++){
+    RegEd = parseInt(prompt("Ingresar la edad de la persona #"+n));
+    AcumEd = AcumEd+RegEd;
+}
+console.log(`El promedio de las edades de ${NumPer} personas es : ${AcumEd/NumPer}`);
+alert(`El promedio de las edades de ${NumPer} personas es : ${AcumEd/NumPer}`);
