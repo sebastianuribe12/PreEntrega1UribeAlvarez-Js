9ea63443493e2a47d221a37dcda370b412c1de81
sebastianuribe12 <urisbe@gmail.com>
2023-07-24 11:59:11 -0500

    Creamos simulador de costo por noche para un hotel

--- java.js
+++ java.js
@@ -1,29 +1,38 @@
-alert("Calculador de promedio de edades ✌️\n -----------por Sebastian Uribe Alvarez-----------");
+alert("Bienvenid@ al calculador de coste de hotel  ✌️\n -----------por Sebastian Uribe Alvarez----------- ");
 
-let NumPer = parseInt(prompt("cuantas personas registrara"));//numero de personas 
+let NumPer = parseInt(prompt("cuantas personas registrara\n (personas por habitacion 4)"));//numero de personas que estaran hospedadas
 if(Number.isNaN(NumPer) == true){
     while(Number.isNaN(NumPer) == true){
-        alert("Porfavor ingrese algun valor Valido");
-        NumPer = parseInt(prompt("cuantas personas registrara"));
+        alert("Por favor ingrese algun valor Valido");
+        NumPer = parseInt(prompt("cuantas personas se Hospedaran"));
     }
 }
-let RegEd;//Registro de las edades 
-let AcumEd = 0; //acumulador de edades 
-let n;//ciclo
 
-for(n=1; n<=NumPer;n++){
-    RegEd = parseInt(prompt("Ingresar la edad de la persona #"+n));
-    
-    if(Number.isNaN(RegEd) == true){
-        while(Number.isNaN(RegEd) == true){
-            alert("Porfavor ingrese algun valor");
-            RegEd = parseInt(prompt("Ingresar la edad de la persona #"+n));
-            
-        }
+
+let Regdia = parseInt(prompt("cuantos dias se quedaran"));//Registro de los dias
+if(Number.isNaN(Regdia) == true){
+    while(Number.isNaN(Regdia) == true){
+        alert("Por favor ingrese algun valor Valido");
+        Regdia = parseInt(prompt("cuantas dias se quedaran"));//VALIDACION DE LOS DATOS
     }
-    AcumEd = AcumEd+RegEd;
-    RegEd = NaN;
-    
 }
-console.log(`El promedio de las edades de ${NumPer} personas es : ${AcumEd/NumPer}`);
-alert(`El promedio de las edades de ${NumPer} personas es : ${AcumEd/NumPer}`);
+let cost; // costo por noche
+let NumH//numero de habitaciones
+let costViaje
+let aux
+cost = 30;
+
+if( NumPer >= 4){ //VER SI SE PUEDE DIVIDIR ENTERO
+    if( NumPer % 4 == 0){
+        NumH = NumPer/4;
+    }
+    else{
+        aux=(NumPer)/4;// tomamos el numero no entero y lo convertimos a entero le sumamos 1 a las habitaciones
+        NumH = aux -((NumPer%4)/4)+1;
+    }
+}
+else{
+    NumH =1;
+}
+console.log(`El precio total del viaje de ${NumPer} personas por ${Regdia} noches en ${NumH} habitaciones es : ${(NumH*Regdia)*cost} USD \n(COSTO POR HABITACION 30 USD)`);
+alert(`El precio total del viaje de ${NumPer} personas por ${Regdia} noches en ${NumH} habitaciones es : ${(NumH*Regdia)*cost} USD \n(COSTO POR HABITACION 30 USD)`);
